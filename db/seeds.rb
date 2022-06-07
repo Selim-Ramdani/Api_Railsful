9e1cd5dff48f5ad8ba87ce44a6272803b5d51d85
curlyroots <selimcodeur@gmail.com>
2022-06-07 17:30:10 +0200

    back-end ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Article.destroy_all
+User.destroy_all
+
+10.times do
+  User.create(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+end
+
+30.times do
+  Article.create(
+    title: Faker::BossaNova.artist,
+    content: Faker::BossaNova.song,
+    user: User.all.sample
+  )
+end
